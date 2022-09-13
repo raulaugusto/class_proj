76dd6431d01aa6a6c686aeef7158e62f9613eba9
Raul Santos <contatoraulaugusto@gmail.com>
2022-09-13 08:43:01 -0300

    001

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
-import { Dados } from './dados/dados';
-
+import { Cliente } from './cliente';
+import { HttpClient } from '@angular/common/http';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+ selector: 'app-root',
+ templateUrl: './app.component.html',
+ styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'class_proj';
-
-  dados = new Dados();
-
-  increase(){
-    this.dados.numero++;
-  }
+ title = 'form-tutorial';
+ cliente: Cliente = new Cliente();
+ constructor(private http: HttpClient) {
+ }
+ VerificaCEP() {
+ this.http.get(`https://viacep.com.br/ws/${this.cliente.CEP}/json/`).toPromise().then(
+ data => {
+ const dado: any = data;
+ this.cliente.Endereco = dado.logradouro;
+ }
+ );
+ }
+ salvar() {
+ console.log(this.cliente);
+ }
 }
